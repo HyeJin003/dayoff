003d00b6bf749f7b70f9725960f5eb153f9b1276
HyeJin003 <rkddkwnl003@gmail.com>
2025-10-10 15:16:58 +0900

    프로젝트 count 앱 만들기 완료

---
+++ project1/src/components/Viewer.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const Viewer = ({count}) => {
+
+  return (
+    <>
+      <div>
+        현재 카운트:
+        <h2>{count}</h2>
+      </div>
+    </>
+  )
+}
+export default Viewer;
